92491ed61446d1b6bf39ae2992870014c1b89e0c
João Victor Paes Barreto <joao.paes.oficial@gmail.com>
2021-05-10 18:36:46 -0300

    Criação da (base e task_list).html e classes

--- ToDo/base/urls.py
+++ ToDo/base/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from .views import TaskList, TaskCreate, TaskUpdate, CustomLoginView, RegisterPage
+from . import views
 
 urlpatterns = [
     path('', TaskList.as_view(), name= 'tasks'),
+    path('task-create/', TaskCreate.as_view(), name= 'task-create'),
+    path('task-update/<int:pk>/', TaskUpdate.as_view(), name= 'task-update'),
+    path('deletetask/<int:id>', views.deleteTask, name= 'delete-task'),
 
 ]
--- ToDo/base/views.py
+++ ToDo/base/views.py
@@ -1,4 +1,8 @@
+from django.shortcuts import redirect, get_object_or_404
 from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView, UpdateView
+from django.urls import reverse_lazy
+from django.contrib import messages
 
 from django.contrib.auth.mixins import LoginRequiredMixin
 
@@ -10,3 +14,21 @@ from .models import Task
 class TaskList(LoginRequiredMixin, ListView):
     model = Task
     context_object_name = 'tasks'
+
+
+class TaskCreate(LoginRequiredMixin, CreateView):
+    model = Task
+    fields =  ['title', 'description', 'complete']
+    success_url = reverse_lazy('tasks')
+
+
+class TaskUpdate(LoginRequiredMixin, UpdateView):
+    model = Task
+    fields = ['title', 'description', 'complete']
+    success_url = reverse_lazy('tasks')
+
+def deleteTask(request, id):
+    task = get_object_or_404(Task, pk= id)
+    task.delete()
+    messages.info(request, "Tarefa deletada com sucesso.")
+    return redirect("/")

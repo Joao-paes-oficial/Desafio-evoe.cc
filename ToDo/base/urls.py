913aacef50f8abf25574be111a24ab1144f7d09e
João Victor Paes Barreto <joao.paes.oficial@gmail.com>
2021-05-10 18:44:20 -0300

    Criação do (login, register e task_form).html e classes

--- ToDo/base/urls.py
+++ ToDo/base/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
-from .views import TaskList, TaskCreate, TaskUpdate, CustomLoginView, RegisterPage
 from . import views
+from .views import TaskList, TaskCreate, TaskUpdate, CustomLoginView, RegisterPage
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
+    path('login/', CustomLoginView.as_view(), name= 'login'),
+    path('logout/', LogoutView.as_view(next_page= 'login'), name= 'logout'),
+    path('register/', RegisterPage.as_view(), name= 'register'),
+
     path('', TaskList.as_view(), name= 'tasks'),
     path('task-create/', TaskCreate.as_view(), name= 'task-create'),
     path('task-update/<int:pk>/', TaskUpdate.as_view(), name= 'task-update'),
--- ToDo/base/views.py
+++ ToDo/base/views.py
@@ -1,14 +1,38 @@
 from django.shortcuts import redirect, get_object_or_404
 from django.views.generic.list import ListView
-from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.edit import CreateView, UpdateView, FormView
 from django.urls import reverse_lazy
 from django.contrib import messages
 
+from django.contrib.auth.views import LoginView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 
 from .models import Task
 
 
+
+class CustomLoginView(LoginView):
+    template_name = 'base/login.html'
+    fields = '__all__'
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy('tasks')
+
+
+class RegisterPage(FormView):
+    template_name = 'base/register.html'
+    form_class = UserCreationForm
+    redirect_authenticated_user = True
+    success_url = reverse_lazy('tasks')
+
+    def form_valid(self, form):
+        user = form.save()
+        if user is not None:
+            login(self.request, user)
+        return super(RegisterPage, self).form_valid(form)
 
 
 class TaskList(LoginRequiredMixin, ListView):
